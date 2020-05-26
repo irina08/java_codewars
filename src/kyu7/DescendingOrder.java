425af0e37ff0af090fd3fa0bca24d96782310811
irina08 <irina_ger@yahoo.com>
2020-05-25 22:24:21 -0700

    added kyu7 DescendingOrder.java

---
+++ src/kyu7/DescendingOrder.java
@@ -0,0 +1,11 @@
+package kyu7;
+
+import java.util.Arrays;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        char[] tempArray = String.valueOf(num).toCharArray();
+        Arrays.sort(tempArray);
+        return Integer.parseInt(new StringBuilder(new String(tempArray)).reverse().toString());
+    }
+}
