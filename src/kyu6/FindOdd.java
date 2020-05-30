bb8a984c8c6350fc78fcd40104f56052501a3615
irina08 <irina_ger@yahoo.com>
2020-05-30 02:20:49 -0700

    added second solution to FindOdd.java

--- src/kyu6/FindOdd.java
+++ src/kyu6/FindOdd.java
@@ -19,3 +19,25 @@ public class FindOdd {
     }
 }
 
+/*
+//second solution
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindOdd {
+  public static int findIt(int[] a) {
+    Map<Integer, Integer> counts = new HashMap<>(a.length);
+    for(int i : a) {
+      if(!counts.containsKey(i)) counts.put(i, 1);
+      else counts.put(i, counts.get(i) + 1);
+    }
+    for(Map.Entry<Integer, Integer> entry : counts.entrySet())
+        if(entry.getValue() % 2 == 1)
+            return entry.getKey();
+    return 0;
+  }
+}
+ */
+
+
