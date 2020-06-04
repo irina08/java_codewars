3c29e5008208500e7fa4a321f0e326677b63fc7c
irina08 <irina_ger@yahoo.com>
2020-06-03 23:11:51 -0700

    added StringToCamelCase.java

---
+++ src/kyu6/StringToCamelCase.java
@@ -0,0 +1,21 @@
+/* Complete the method/function so that it converts dash/underscore
+delimited words into camel casing. The first word within the output
+should be capitalized only if the original word was capitalized
+(known as Upper Camel Case, also often referred to as Pascal case).
+Examples
+toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
+toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
+*/
+
+import java.lang.StringBuilder;
+
+class StringToCamelCase{
+
+  static String toCamelCase(String s){
+    String[] words = s.split("[-_]");
+    StringBuilder sb = new StringBuilder(words[0]);
+    for (int i = 1; i < words.length; i++)
+        sb.append(words[i].substring(0, 1).toUpperCase()).append(words[i].substring(1));
+    return sb.toString();
+  }
+}
