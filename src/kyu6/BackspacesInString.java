07c4729f5f591dab0788b03e21954a86af11a3ec
irina08 <irina_ger@yahoo.com>
2020-06-05 20:38:18 -0700

    added BackspacesInJava.java

---
+++ src/kyu6/BackspacesInString.java
@@ -0,0 +1,16 @@
+/* Assume "#" is like a backspace in string. This means that string
+"a#bc#d" actually is "bd"
+Your task is to process a string with "#" symbols.
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+ */
+
+public class BackspacesInString {
+    public String cleanString(String s) {
+        while (s.matches(".*#.*")) s = s.replaceFirst(".?#", "");
+        return s;
+    }
+}
