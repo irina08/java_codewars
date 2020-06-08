9a1df9ad158e179cc96ad1f7401346a1cb627a08
irina08 <38026611+irina08@users.noreply.github.com>
2020-06-08 11:54:20 -0700

    Update CountTheDays.java

--- src/kyu6/CountTheDays.java
+++ src/kyu6/CountTheDays.java
@@ -19,20 +19,4 @@ public class CountTheDays {
     }
 }
 
-//Second solution
- /*
- import java.util.Date;
-
- import java.util.concurrent.TimeUnit;
-
- public class Day{
-
-     public static String countDays(Date d){
-         Date now = new Date();
-         if (now.equals(d)) return "Today is the day!";
-         else if (now.after(d)) return "The day is in the past!";
-         else return TimeUnit.DAYS.convert(d.getTime() - now.getTime(), TimeUnit.MILLISECONDS) + " days";
-     }
- }
-  */
 
