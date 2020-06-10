6c3d5c94149463e8556b6609f1c448a1e1f9695d
irina08 <irina_ger@yahoo.com>
2020-06-10 13:37:43 -0700

    added second solution to CountDuplicates.java

--- src/kyu6/CountDuplicates.java
+++ src/kyu6/CountDuplicates.java
@@ -28,3 +28,19 @@ public class CountingDuplicates {
     }
 }
 
+//SecondWay
+/*
+public class CountingDuplicates {
+  public static int duplicateCount(String text) {
+    int ans = 0;
+    text = text.toLowerCase();
+    while (text.length() > 0) {
+      String firstLetter = text.substring(0,1);
+      text = text.substring(1);
+      if (text.contains(firstLetter)) ans ++;
+      text = text.replace(firstLetter, "");
+    }
+    return ans;
+  }
+}
+ */
