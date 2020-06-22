6c650f4f2a5ad9241c74f58770e0c64ffa6444f0
irina08 <irina_ger@yahoo.com>
2020-06-22 09:59:51 -0700

    added second solution to DeleteOccurrences.java

--- src/kyu6/DeleteOccurrences.java
+++ src/kyu6/DeleteOccurrences.java
@@ -49,3 +49,26 @@ public class DeleteOccurrences{
     }
 }
 
+
+//second solution
+/*
+import java.util.*;
+
+public class EnoughIsEnough {
+
+  public static int[] deleteNth(int[] elements, int max) {
+
+    if (max < 1) return new int[0];
+
+    final HashMap<Integer,Integer> map = new HashMap<>();
+    final List<Integer> list = new ArrayList<>();
+
+    for (final Integer i : elements) {
+      final Integer v = map.put(i, map.getOrDefault(i, 0) + 1);
+      if (v == null || v < max) list.add(i);
+    }
+
+    return list.stream().mapToInt(i->i).toArray();
+  }
+}
+ */
