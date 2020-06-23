9095a062097876106693b5f4fb332e7ff8f643d3
irina08 <irina_ger@yahoo.com>
2020-06-23 02:05:34 -0700

    added second solution to WhichAreIn.java

--- src/kyu6/WhichAreIn.java
+++ src/kyu6/WhichAreIn.java
@@ -20,3 +20,30 @@ public class WhichAreIn {
         return result.toArray(new String[result.size()]);
     }
 }
+
+
+//second solution
+/*
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class WhichAreIn {
+  public static String[] inArray(String[] array1, String[] array2) {
+     Set<String> result = new HashSet<>();
+
+     for(String a1 : array1) {
+       for(String a2 : array2) {
+         if(a2.contains(a1)) {
+           result.add(a1);
+           break;
+         }
+       }
+     }
+     String[] resultArray = result.toArray(new String[result.size()]);
+     Arrays.sort(resultArray);
+     return resultArray;
+  }
+}
+ */
+
