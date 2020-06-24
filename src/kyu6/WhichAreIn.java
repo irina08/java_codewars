35655da90e2193ac2160cc30589f479412760b99
irina08 <irina_ger@yahoo.com>
2020-06-24 00:45:13 -0700

    added solution with stream to WhichAreIn.java

--- src/kyu6/WhichAreIn.java
+++ src/kyu6/WhichAreIn.java
@@ -47,3 +47,18 @@ public class WhichAreIn {
 }
  */
 
+//solutions with stream
+/*
+import java.util.Arrays;
+
+public class WhichAreIn {
+  public static String[] inArray(String[] array1, String[] array2) {
+    return Arrays.stream(array1)
+      .filter(str ->
+        Arrays.stream(array2).anyMatch(s -> s.contains(str)))
+      .distinct()
+      .sorted()
+      .toArray(String[]::new);
+  }
+}
+ */
