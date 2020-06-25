c291f44a37a7bdf1ef0908457282c756235a7f3c
irina08 <irina_ger@yahoo.com>
2020-06-25 00:10:54 -0700

    added second solution to PersistentBigger.java

--- src/kyu6/PersistentBigger.java
+++ src/kyu6/PersistentBigger.java
@@ -28,3 +28,21 @@ class PersistentBigger {
         return counter;
     }
 }
+
+//second solution
+/*
+class Persist {
+  public static int persistence(long n) {
+    long m = 1, r = n;
+
+    if (r / 10 == 0)
+      return 0;
+
+    for (r = n; r != 0; r /= 10)
+      m *= r % 10;
+
+    return persistence(m) + 1;
+
+  }
+}
+ */
