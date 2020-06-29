bd24592b0d216dd2b4dafcfb1db306c21dc2c70f
irina08 <irina_ger@yahoo.com>
2020-06-28 21:50:04 -0700

    added second solution to EncryptIt.java

--- src/kyu6/EncryptIt.java
+++ src/kyu6/EncryptIt.java
@@ -35,3 +35,15 @@ public class EncryptIt {
     }
 }
 
+//second solution with Stream
+/*
+public class Kata {
+    public static String encryptThis(String text) {
+        return Arrays.stream(text.split(" "))
+                .map(w->w.length()>2?(int)w.charAt(0)+w.substring(w.length()-1)+w.substring(2, w.length()-1)+w.substring(1,2):
+                    w.length()>1?(int)w.charAt(0)+w.substring(1):
+                    w.length()==1?(int)w.charAt(0)+"":"")
+                .collect(Collectors.joining(" "));
+    }
+}
+ */
